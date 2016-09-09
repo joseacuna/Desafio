87b99bf741418ab3915af21dbf7e1c1fa975e9e3
Phobos <Phobos@Phobos-PC>
2016-09-09 20:25:07 -0300

    inicio Del Desafio

---
+++ src/Desafio/Principal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Desafio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Phobos
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        //Crear La Estructura.
+
+        int indices = (int) (Math.random() * 9 + 1);
+
+         ArrayList<ArrayList<Integer>> matris = new ArrayList<ArrayList<Integer>>();
+         
+         //creo elemento vacio
+         matris.add(new ArrayList<Integer>());
+         
+         
+         
+         
+    
+
+        
+        
+    }
+    
+}
