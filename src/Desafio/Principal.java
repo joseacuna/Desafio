84c15535ce276e534f464a160238054eb975f145
joseacuna <joseacunaacuna@gmail.com>
2016-09-23 11:01:28 -0300

    VectorCaminos Aleatorios

--- src/Desafio/Principal.java
+++ src/Desafio/Principal.java
@@ -16,8 +16,8 @@ public class Principal {
     public static void main(String[] args) {
         //Crear La Estructura.
 
-        int filas = (int) (Math.random() * 9 + 1);//rows
-        int columnas = (int) (Math.random() * 9 + 1);//colums
+        int filas = (int) (Math.random() * 5 + 1);//rows
+        int columnas = (int) (Math.random() * 5 + 1);//colums
         //int datos = (int) (Math.random() * 9 + 1);
         System.out.println("Filas: " + filas + " Columnas : " + columnas);
         System.out.println("---------------------------------");
@@ -42,12 +42,29 @@ public class Principal {
         }
 
         System.out.println("---------------------------------");
-        System.out.println("CAMINO");
-        System.out.println("---------------------------------");
+
         //algoritmo
         ArrayList<Integer> vectorCamino = new ArrayList<Integer>();
 
-        
+        int rndFila = 0;
+        int rndColumna = 0;
+        int tamano = (filas + columnas) - 1;
+        for (int i = 0; i < tamano; i++) {
+
+            rndFila = (int) (Math.random() * filas);
+            rndColumna = (int) (Math.random() * columnas);
+            if(rndFila!=rndColumna){
+            int valor = matris.get(rndFila).get(rndColumna);
+            System.out.println("M[" + rndFila + "][" + rndColumna + "] :" + valor);
+            vectorCamino.add(valor);
+        }}
+         System.out.println("");
+        System.out.println("CAMINO");
+        System.out.println("---------------------------------");
+        for (int i = 0; i < vectorCamino.size(); i++) {
+            System.out.print("{" + vectorCamino.get(i) + "}");
+        }
+
     }//endMain
 
 }
